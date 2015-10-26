6729ceb10441f44b506609971214ccdac32b7095
Rich Choy <rich@it-consultis.com>
2015-10-26 21:32:30 +0800

    add new-york to example url enumerator

--- files/resources/spark/urls.php
+++ files/resources/spark/urls.php
@@ -1,8 +1,14 @@
 <?php
 
-return function(ArrayIterator $urls, $routes, $app)
+/**
+ * @param \ArrayIterator $urls
+ * @param \ArrayIterator $routes
+ * @param \Illuminate\Contracts\Container\Container $app
+ */
+return function($urls, $routes, $app)
 {
     $urls->append('/cities');
     $urls->append('/cities/san-francisco');
+    $urls->append('/cities/new-york');
 };
 
